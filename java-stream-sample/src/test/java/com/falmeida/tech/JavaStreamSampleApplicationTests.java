bb9f9337f41aa91ed864d4a14e7e9d7786ece970
Almeida <falmeida.tech@gmail.com>
2020-05-11 12:04:58 -0400

    Initial commit.

---
+++ java-stream-sample/src/test/java/com/falmeida/tech/JavaStreamSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falmeida.tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStreamSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
